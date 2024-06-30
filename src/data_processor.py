bb2f51299f9df8910543adc3a89b42028cb3fe49
LL145 <44174238+LL145@users.noreply.github.com>
2024-06-30 21:38:49 +0000

    1

--- src/data_processor.py
+++ src/data_processor.py
@@ -20,7 +20,6 @@ class DataProcessor:
     def process_ma(self):
         df = self.config.read_data_filled()
         windows = {
-            'MA7': 5,
             'MA1M': 20,
             'MA3M': 65,
             'MA6M': 130,
@@ -34,14 +33,14 @@ class DataProcessor:
                 ma_column_name = f'{column}-{key}'
                 ma_columns[ma_column_name] = df[column].rolling(window=window, min_periods=1).mean()
                 ratio_column_name = f'{column}-{key}-R'
+                diff_column_name = f'{column}-{key}-D'
                 ma_columns[ratio_column_name] = df[column] / ma_columns[ma_column_name]
+                ma_columns[diff_column_name] = df[column] - ma_columns[ma_column_name]
         df = pd.concat([df, pd.DataFrame(ma_columns, index=df.index)], axis=1)
+        
         # ===============额外特征
         df['US_GDP-PERCAPITA'] = df['US_GDP']/df['US_POPULATION']
-        df['MONTH'] = df.index.month
-        df['WEEKDAY'] = df.index.weekday + 1
-        df['LEAPYEAR'] = (df.index.year % 4 == 0).astype(int)
-        # =======================
+
         return df
     
     def process(self):
@@ -49,45 +48,45 @@ class DataProcessor:
         new_df = pd.DataFrame(index=data.index)
 
         # 直接复制
-        base_columns = ['SP500_TR', 'SP500_VOLUME','FEDFUNDS', 'GS1', 'GS2', 'GS5', 
+        base_columns = ['SP500_TR', 'FEDFUNDS', 'DGS3MO','GS1', 'GS2', 
                         'GS10', 'GS30', 'DXY', 'VIX', 'US_UNEMPLOYMENT', 'SP500_PE',
-                        'SHILLER_PE','SP500_DIVIDEND_YIELD', 'SP500_EARNINGS_YIELD',
-                        'MONTH', 'WEEKDAY', 'LEAPYEAR', 'HIGH_YIELD_SPREAD']
+                        'HIGH_YIELD_SPREAD',
+                        'T10Y3M','T10Y2Y', 'T10YIE','CONSUMER_SENTIMENT','RECESSION', 'NFCI']
         for base in base_columns:
             new_df[base] = data[base]
 
-        # SP500每日涨幅
-        new_df['SP500_TR_CHANGE'] = new_df['SP500_TR'].pct_change() * 100
-        # M1, M2 /GDP
+        # M1, M2, 基础货币 /GDP
         new_df['M1_GDP_RATIO'] = data['US_M1']/data['US_GDP']
         new_df['M2_GDP_RATIO'] = data['US_M2']/data['US_GDP']
+        new_df['US_MONETARY_BASE_GDP_RATIO'] = data['US_MONETARY_BASE']/data['US_GDP']
 
-        # GDP,人口 增长率
-        base_columns = ['US_GDP', 'US_POPULATION']
-        suffixes = ['MA1Y-R', 'MA3Y-R', 'MA5Y-R']
-        for base in base_columns:
-            for suffix in suffixes:
-                column_name = f'{base}-{suffix}'
-                if column_name in data.columns:
-                    new_df[column_name] = data[column_name]
+        # WTI_BRENT_SPREAD_RATIO
+        new_df['WTI_BRENT_SPREAD_RATIO'] = (data['WTI_OIL'] - data['BRENT_OIL']) / data['BRENT_OIL']
+
+        # 美联储总资产/GDP
+        new_df['WALCL_GDP'] = data['WALCL'] / data['US_GDP']
+        # 隔夜逆回购/GDP
+        new_df['RRPONTSYD_GDP'] = data['RRPONTSYD'] / data['US_GDP']
+
         # ==========================
-
-        base_columns = ['SP500_TR', 'SP500_VOLUME', 'RUSSELL2000', 'RUSSELL2000', 'VGTSX','GOLD', 'US_CPI', 'US_PCE', 'US_PPI','US_HOUSE']
-        suffixes = ['MA7-R', 'MA1M-R','MA3M-R' ,'MA6M-R','MA1Y-R', 'MA3Y-R', 'MA5Y-R']
+        base_columns = ['SP500_TR', 'RUSSELL2000', 'NASDAQ', 'VGTSX', 'US_CPI', 'US_PCE', 'US_PPI', 'DXY', 'RRPONTSYD', 'WALCL','US_GDP', 'US_POPULATION', 'US_MONETARY_BASE', 'US_M1', 'US_M2', 'US_HOUSE', 'BRENT_OIL', 'GOLD', 'US_UNEMPLOYMENT','T10YIE', 'FEDFUNDS', 'GS1', 'GS2', 'GS10', 'GS30',
+                       'GDPC1','PAYEMS','HIGH_YIELD_SPREAD','DHHNGSP','DGS3MO']
+        suffixes = ['MA1M-R','MA3M-R' ,'MA6M-R','MA1Y-R', 'MA3Y-R', 'MA5Y-R']
         for base in base_columns:
             for suffix in suffixes:
                 column_name = f'{base}-{suffix}'
                 if column_name in data.columns:
                     new_df[column_name] = data[column_name]
-        
+
+        # =========================
+        base_columns = ['T10Y3M', 'T10YIE', 'FEDFUNDS', 'GS1', 'GS2', 'GS10', 'GS30','HIGH_YIELD_SPREAD', 'US_UNEMPLOYMENT', 'DGS3MO']
+        suffixes = ['MA3M-D' ,'MA6M-D','MA1Y-D', 'MA3Y-D', 'MA5Y-D']
+        for base in base_columns:
+            for suffix in suffixes:
+                column_name = f'{base}-{suffix}'
+                if column_name in data.columns:
+                    new_df[column_name] = data[column_name]
         # ==========================
         new_df = new_df.loc['2005-08-01':]
         
         self.config.save_data_processed(new_df)
-
-
-
-
-        
-
-
--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 # src/main.py
 import os
 import logging
-from datetime import datetime, timedelta
-import pytz
+from datetime import datetime, time, timedelta
 from data_fetcher import DataFetcher
 from config_reader import ConfigReader
 from data_processor import DataProcessor
-from trainer import Trainer
-
-
+import pytz
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -17,36 +14,21 @@ def main():
     config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'config.toml')
     global_config = ConfigReader(config_path)
     data_fetcher = DataFetcher(global_config)
-
-
     start = datetime(1995, 1, 1)
-    end = datetime.now()
-    
+    end = get_ny_date_without_timezone()
     data_fetcher.fetch_all(start, end)
     data_processor = DataProcessor(global_config)
     data_processor.clean_and_fill_data()
     data_processor.process()
 
-def get_new_york_datetime_naive():
-    # 定义纽约时区
-    new_york_tz = pytz.timezone('America/New_York')
-
-    # 获取当前纽约时间
-    new_york_time = datetime.now(new_york_tz)
-
-    # 获取纽约时间的下午5点
-    five_pm = new_york_time.replace(hour=17, minute=0, second=0, microsecond=0)
-
-    # 判断当前时间是否超过下午5点
-    if new_york_time > five_pm:
-        result_datetime = new_york_time  # 今天的日期时间
-    else:
-        result_datetime = new_york_time - timedelta(days=1)  # 昨天的日期时间
-
-    # 将带时区的datetime对象转换为不带时区的datetime对象
-    naive_result_datetime = result_datetime.replace(tzinfo=None)
-
-    return naive_result_datetime
+def get_ny_date_without_timezone():
+    new_york_timezone = pytz.timezone('America/New_York')
+    now_ny = datetime.now(new_york_timezone)
+    target_date = now_ny if now_ny.time() >= time(16, 0) else now_ny - timedelta(days=1)
+    target_date_without_tz = datetime.combine(target_date.date(), time(23, 0))
+    while target_date_without_tz.weekday() >= 5:
+        target_date_without_tz -= timedelta(days=1)
+    return target_date_without_tz
 
 if __name__ == "__main__":
   main()
